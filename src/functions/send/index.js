c1d140fbcf36020492350aedccb2eb4937ede040
Tomas <tomasholderness@gmail.com>
2017-09-18 11:48:52 -0400

    added config to send message

--- src/functions/send/index.js
+++ src/functions/send/index.js
@@ -1,12 +1,32 @@
 import twitter from '../../lib/twitter/';
 import messages from '../../lib/twitter/messages';
-// TODO - send message
+
+const config = {
+  oauth: {
+    consumer_key: process.env.TWITTER_CONSUMER_KEY,
+    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+    token: process.env.TWITTER_ACCESS_TOKEN_KEY,
+    token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+  },
+  app: {
+    consumer_secret: process.env.TWITTER_APP_CONSUMER_SECRET,
+    twitter_user_id: '905602080252977152', // @riskmapus bot,
+    default_lang: process.env.DEFAULT_LANG,
+    twitter_endpoint: `https://api.twitter.com/1.1/direct_messages/events/new.json`,
+  },
+  server: {
+    card_endpoint: `https://cards.riskmap.us/flood/`,
+    card_api: `https://3m3l15fwsf.execute-api.us-west-2.amazonaws.com/prod/cards`,
+    api_key: process.env.SERVER_API_KEY,
+  },
+};
+
 module.exports.twitterReply = (event, context, callback) => {
   // This module listens in to SNS Twitter topic and reads the message published
   let message = JSON.parse(event.Records[0].Sns.Message);
   console.log('Message received from SNS topic: ' + message);
   // Prepare message
-  let msg = messages.thanks('en', message.username, message.report_id);
+  let msg = messages(config).thanks('en', message.username, message.report_id);
   // Send message to user
   twitter.sendMessage(msg)
     .then((response) => console.log('Message sent.'))
