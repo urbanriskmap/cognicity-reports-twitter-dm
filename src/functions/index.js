17bff8edcac711c71e443157a6a2233b744efcf7
abrahamq <abrahamquintero2323@gmail.com>
2018-02-09 18:51:33 -0500

    Revert "began work on facebook ad buy bot", forgot to change remote.
    This reverts commit 675eeb77fdf841715ad8910a021f1f49ec0eb56b.

--- src/functions/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config();
-
-module.exports.buyFacebookGeolocationAd = (event, context, callback) => {
-  let message = JSON.parse(event.Records[0].Sns.Message);
-  //Get geolocation from sns message
-  //Create
-};
