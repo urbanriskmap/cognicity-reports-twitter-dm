eb791f47019f32e265a5c5f1d55281f96e6ecec5
Tomas <tomasholderness@gmail.com>
2018-06-01 13:21:29 -0400

    add logging

--- src/functions/receive/index.js
+++ src/functions/receive/index.js
@@ -42,7 +42,9 @@ export default async (event, context, callback) => {
       // Check request is authentic
       const headers = await Joi.validate(event.headers, _dmHeaderSchema,
         {stripUnknown: true});
+        console.log(headers)
       const payload = await Joi.validate(event.body, _dmBodySchema);
+      console.log(headers);
       const signed = twitter.signatureValidation(
         headers['X-Twitter-Webhooks-Signature'], payload);
       // Async loop through incoming DMs
