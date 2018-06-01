8a5585c011985b62230cad4f4868e5324d93c641
Tomas <tomasholderness@gmail.com>
2018-06-01 10:14:37 -0400

    debugging tw client app

--- src/functions/receive/index.js
+++ src/functions/receive/index.js
@@ -37,6 +37,7 @@ export default async (event, context, callback) => {
     } else if (event.httpMethod === 'POST') {
       // Async loop through incoming DMs
       const payload = await Joi.validate(event.body, _dmBodySchema);
+      console.log(JSON.stringify(payload));
       if (payload.direct_message_events) {
         // Loop messages (synchronous)
         for (const item of payload.direct_message_events) {
